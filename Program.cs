648039641d0950f5e46616932fc2175265b5e97d
Seb.Fxs <Fuchs.basti@gmail.com>
2022-08-14 20:42:59 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+//ascii art by https://ascii.co.uk/art/hangman
+
+var renderer = new hangman.Render();
+var wordlist = new hangman.Wordlist();
+
+List<char> guessed = new() {};
+
+Random rd = new Random();
+string word = wordlist.Words[rd.Next(0, wordlist.Words.Count - 1)];
+
+bool completed = false;
+int wrongAnswers = 0;
+string message = "";
+
+renderer.RenderScreen(0, word, guessed,"");
+while (!completed)
+{
+    message = "";
+    Console.Write("Buchstabe Eingeben: ");
+    string val = Console.ReadLine();
+    Console.WriteLine(val);
+    if (val == word)
+    {
+        completed = true;
+        message = "Du hast gewonnen!";
+    }
+    else if (val == null || val.Count() > 1)
+    {
+        message = "Bitte Genau 1 Buchtstabe oder das ganze Wort eingeben!";
+        wrongAnswers++;
+    }
+    else
+    {
+        if (!word.Contains(val.ToCharArray().First()) || guessed.Contains(val.ToCharArray().First())) {
+            wrongAnswers++;
+        }
+        guessed.Add(val.ToCharArray().First());
+    }
+
+    if (wrongAnswers == renderer.hangmanStages.Count() - 1)
+    {
+        completed = true;
+        message = "Du hast verloren!";
+    }
+    else if (word.All(c => { return guessed.Contains(c); }))
+    {
+        completed = true;
+        message = "Du hast gewonnen!";
+    }
+
+    renderer.RenderScreen(wrongAnswers, word, guessed, message);
+}
---
+++ Render.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangman
+{
+    internal class Render
+    {
+        public Render(){}
+
+        public string hangmanLogo = ""
+            + " _\n"
+            + "| |     <github link pending>\n"
+            + "| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  \n"
+            + "| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ \n"
+            + "| | | | (_| | | | | (_| | | | | | | (_| | | | |   Von ***REMOVED*** (Github: Fuchsi2)\n"
+            + "|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|   Im Rahmen eines Praktikums bei ***REMOVED***\n"
+            + "                    __/ |\n"
+            + "                   |___/   (nur englische Wörter)\n";
+
+        public List<string> hangmanStages = new()
+            {
+              "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + "\n"
+            + "    _____\n",
+              "\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "    _|___\n",
+              "      _______\n"
+            + "     |/\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "    _|___\n",
+              "      _______\n"
+            + "     |/      |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "    _|___\n",
+              "      _______\n"
+            + "     |/      |\n"
+            + "     |      (_)\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "     |\n"
+            + "    _|___\n",
+              "      _______\n"
+            + "     |/      |\n"
+            + "     |      (_)\n"
+            + "     |       |\n"
+            + "     |       |\n"
+            + "     |\n"
+            + "     |\n"
+            + "    _|___\n",
+              "      _______\n"
+            + "     |/      |\n"
+            + "     |      (_)\n"
+            + "     |      \\|/\n"
+            + "     |       |\n"
+            + "     |\n"
+            + "     |\n"
+            + "    _|___\n",
+              "      _______\n"
+            + "     |/      |\n"
+            + "     |      (_)\n"
+            + "     |      \\|/\n"
+            + "     |       |\n"
+            + "     |      / \\\n"
+            + "     |\n"
+            + "    _|___\n"
+            };
+
+        public void RenderScreen(int stage, string word, List<char> guessedChars, string message)
+        {
+            Console.Clear();
+            Console.WriteLine(hangmanLogo);
+            Console.WriteLine("\n");
+            Console.Write("Wort: ");
+            for (int i = 0; i < word.Count(); i++)
+            {
+                if (guessedChars.Contains(word[i]))
+                {
+                    Console.Write(word[i] + " ");
+                } else
+                {
+                    Console.Write("_ ");
+                }
+            }
+            Console.WriteLine("\n\n");
+            Console.WriteLine(hangmanStages[stage]);
+            Console.WriteLine("\n");
+            Console.WriteLine(message);
+        }
+    }
+}
